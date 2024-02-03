769cd0ca19359e7db55015c726fc57f02e177beb
woojung007 <eeeeve007@gmail.com>
2024-02-03 19:43:07 +0900

    note, todo 구현하기

--- src/App.ts
+++ src/App.ts
@@ -1,4 +1,6 @@
 import { ImageComponent } from "./components/page/item/image.js";
+import { NoteComponent } from "./components/page/item/note.js";
+import { TodoComponent } from "./components/page/item/todo.js";
 import { PageComponent } from "./components/page/page.js";
 
 class App {
@@ -11,8 +13,13 @@ class App {
       "Image Title",
       "https://picsum.photos/600/300"
     );
-
     image.attachTo(appRoot, "beforeend");
+
+    const note = new NoteComponent("Note title", "Note Body");
+    note.attachTo(appRoot, "beforeend");
+
+    const todo = new TodoComponent("Todo title", "Todo Item");
+    todo.attachTo(appRoot, "beforeend");
   }
 }
 
