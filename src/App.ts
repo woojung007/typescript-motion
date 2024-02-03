08e6e59431b65a434c72de3802f920d025d907d8
woojung007 <eeeeve007@gmail.com>
2024-02-03 20:01:57 +0900

    youtube 동영상 section

--- src/App.ts
+++ src/App.ts
@@ -1,6 +1,7 @@
 import { ImageComponent } from "./components/page/item/image.js";
 import { NoteComponent } from "./components/page/item/note.js";
 import { TodoComponent } from "./components/page/item/todo.js";
+import { VideoComponent } from "./components/page/item/video.js";
 import { PageComponent } from "./components/page/page.js";
 
 class App {
@@ -20,6 +21,12 @@ class App {
 
     const todo = new TodoComponent("Todo title", "Todo Item");
     todo.attachTo(appRoot, "beforeend");
+
+    const video = new VideoComponent(
+      "Video title",
+      "https://www.youtube.com/watch?v=0Emq5FypiMM&t=2s"
+    );
+    video.attachTo(appRoot, "beforeend");
   }
 }
 
