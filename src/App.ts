1f9c304eff6390cda8606e7f15150526a30a78ba
woojung007 <eeeeve007@gmail.com>
2024-02-12 22:23:19 +0900

    dialog

--- src/App.ts
+++ src/App.ts
@@ -1,4 +1,5 @@
 import { Component } from "./components/component.js";
+import { InputDialog } from "./components/dialog/dialog.js";
 import { ImageComponent } from "./components/page/item/image.js";
 import { NoteComponent } from "./components/page/item/note.js";
 import { TodoComponent } from "./components/page/item/todo.js";
@@ -32,6 +33,22 @@ class App {
       "https://www.youtube.com/watch?v=0Emq5FypiMM&t=2s"
     );
     this.page.addChild(video);
+
+    const imageBtn = document.querySelector("#new-image")! as HTMLButtonElement;
+    imageBtn.addEventListener("click", () => {
+      const dialog = new InputDialog();
+
+      dialog.setOnCloseListener(() => {
+        dialog.removeFrom(document.body);
+      });
+
+      dialog.setOnSubmitListener(() => {
+        // 섹션을 만들어서 페이지에 추가해준다.
+        dialog.removeFrom(document.body);
+      });
+
+      dialog.attachTo(document.body);
+    });
   }
 }
 
