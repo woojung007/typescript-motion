a73dbd11ec88d1cf42cc2a668309e67226c24527
woojung007 <eeeeve007@gmail.com>
2024-02-13 22:35:10 +0900

    styling

--- src/components/page/item/todo.ts
+++ src/components/page/item/todo.ts
@@ -4,8 +4,9 @@ export class TodoComponent extends BaseComponent<HTMLElement> {
   constructor(title: string, todo: string) {
     super(`
         <section class="todo">
-            <h2 class="todo__title"></h2>
-            <input type="checkbox" class="todo-checkbox">
+            <h2 class="page-item__title todo__title"></h2>
+            <input type="checkbox" id="todo-checkbox">
+            <label for="todo-checkbox" class="todo-label"></label>
         </section>
         `);
 
@@ -15,8 +16,8 @@ export class TodoComponent extends BaseComponent<HTMLElement> {
     titleElement.textContent = title;
 
     const todoElement = this.element.querySelector(
-      ".todo-checkbox"
+      ".todo-label"
     )! as HTMLInputElement;
-    todoElement.insertAdjacentText("afterend", todo);
+    todoElement.textContent = todo;
   }
 }
